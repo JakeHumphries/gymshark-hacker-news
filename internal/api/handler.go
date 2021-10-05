3904254de582c85da90537656b22c22012c9b115
Jake Humphries <jake.humphries@gymshark.com>
2021-10-05 14:53:29 +0100

    feat: updated GetAll handler to return payload in JSON

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/JakeHumphries/gymshark-hacker-news/internal/consumer"
 	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
 )
 
 // Handler contains all the business logic for handling requests
@@ -22,6 +24,10 @@ func New(itemRepository consumer.ItemRepository) *Handler {
 
 // GetAll gets all the items in the item repository
 func (h *Handler) GetAll(c echo.Context) (err error) {
-	h.itemRepository.GetAllItems(h.ctx)
-	return nil
+	i, err := h.itemRepository.GetAllItems(h.ctx)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "get all"))
+	}
+
+	return c.JSON(http.StatusOK, i)
 }
