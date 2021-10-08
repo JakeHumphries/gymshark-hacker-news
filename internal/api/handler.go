7fa04eee2b626ba088dabe7b811394a500636f1e
Jake Humphries <jake.humphries@gymshark.com>
2021-10-08 11:59:38 +0100

    refactor: removed ctx from handler struct and pulled it off the echo var instead

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -19,20 +19,18 @@ type ItemReader interface {
 // Handler contains all the business logic for handling requests
 type Handler struct {
 	itemReader ItemReader
-	ctx        context.Context
 }
 
 // New returns a new api handler
 func New(itemReader ItemReader, ctx context.Context) *Handler {
 	return &Handler{
 		itemReader: itemReader,
-		ctx:        ctx,
 	}
 }
 
 // GetAllItems gets all the items in the item repository
 func (h *Handler) GetAllItems(c echo.Context) (err error) {
-	i, err := h.itemReader.GetAllItems(h.ctx)
+	i, err := h.itemReader.GetAllItems(c.Request().Context())
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "handler"))
 	}
@@ -42,7 +40,7 @@ func (h *Handler) GetAllItems(c echo.Context) (err error) {
 
 // GetStories gets all the items in the item repository with the type of story
 func (h *Handler) GetStories(c echo.Context) (err error) {
-	i, err := h.itemReader.GetStories(h.ctx)
+	i, err := h.itemReader.GetStories(c.Request().Context())
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "handler"))
 	}
@@ -52,7 +50,7 @@ func (h *Handler) GetStories(c echo.Context) (err error) {
 
 // GetJobs gets all the items in the item repository with the type of job
 func (h *Handler) GetJobs(c echo.Context) (err error) {
-	i, err := h.itemReader.GetJobs(h.ctx)
+	i, err := h.itemReader.GetJobs(c.Request().Context())
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "handler"))
 	}
