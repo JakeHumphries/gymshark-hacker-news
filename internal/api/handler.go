54afae18fd25229c08bf6102d5876eaf1f4fb899
Jake Humphries <jake.humphries@gymshark.com>
2021-10-15 13:38:20 +0100

    fix: fixed handler to actually work by calling the grpc client

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -2,40 +2,18 @@ package api
 
 import (
 	"context"
-	"fmt"
 	"io"
-	"net/http"
-
-	"github.com/labstack/echo/v4"
-	"google.golang.org/protobuf/types/known/emptypb"
 
 	grpc "github.com/JakeHumphries/gymshark-hacker-news/internal/grpc/mappers"
 	"github.com/JakeHumphries/gymshark-hacker-news/internal/grpc/protobufs"
 	"github.com/JakeHumphries/gymshark-hacker-news/internal/models"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
-
-// ItemReader is an interface for getting items
-type ItemReader interface {
-	GetAllItems(ctx context.Context) ([]models.Item, error)
-	GetStories(ctx context.Context) ([]models.Item, error)
-	GetJobs(ctx context.Context) ([]models.Item, error)
-}
 
 // Stream is a generic interface for grpc client streams
 type Stream interface {
 	Recv() (*protobufs.Item, error)
-}
-
-// Handler contains all the business logic for handling requests
-type Handler struct {
-	client protobufs.HackerNewsClient
-}
-
-// New returns a new api handler
-func New(client protobufs.HackerNewsClient) *Handler {
-	return &Handler{
-		client: client,
-	}
 }
 
 func readStream(s Stream) ([]models.Item, error) {
@@ -53,47 +31,59 @@ func readStream(s Stream) ([]models.Item, error) {
 	return items, nil
 }
 
+// GrpcHandler contains the logic that calls the grpc server
+type GrpcHandler struct {
+	client protobufs.HackerNewsClient
+}
+
+// New returns a new api router
+func NewHandler(client protobufs.HackerNewsClient) *GrpcHandler {
+	return &GrpcHandler{
+		client: client,
+	}
+}
+
 // GetAllItems gets all items in the grpc all stream
-func (h *Handler) GetAllItems(c echo.Context) (err error) {
-	s, err := h.client.All(c.Request().Context(), &emptypb.Empty{})
+func (h GrpcHandler) GetAllItems(c context.Context) ([]models.Item, error) {
+	s, err := h.client.All(c, &emptypb.Empty{})
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, fmt.Sprintf("calling grpc all: %s", err))
+		return nil, errors.Wrap(err, "calling grpc all")
 	}
 
 	items, err := readStream(s)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, fmt.Sprintf("reading grpc all stream: %s", err))
+		return nil, errors.Wrap(err, "reading grpc all stream")
 	}
 
-	return c.JSON(http.StatusOK, items)
+	return items, nil
 }
 
 // GetStories gets all items in the grpc stories stream
-func (h *Handler) GetStories(c echo.Context) (err error) {
-	s, err := h.client.Stories(c.Request().Context(), &emptypb.Empty{})
+func (h GrpcHandler) GetStories(c context.Context) ([]models.Item, error) {
+	s, err := h.client.Stories(c, &emptypb.Empty{})
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, fmt.Sprintf("calling grpc stories: %s", err))
+		return nil, errors.Wrap(err, "calling grpc stories")
 	}
 
 	items, err := readStream(s)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, fmt.Sprintf("reading grpc stories stream: %s", err))
+		return nil, errors.Wrap(err, "reading grpc stories stream")
 	}
 
-	return c.JSON(http.StatusOK, items)
+	return items, nil
 }
 
 // GetJobs gets all items in the grpc jobs stream
-func (h *Handler) GetJobs(c echo.Context) (err error) {
-	s, err := h.client.Jobs(c.Request().Context(), &emptypb.Empty{})
+func (h GrpcHandler) GetJobs(c context.Context) ([]models.Item, error) {
+	s, err := h.client.Jobs(c, &emptypb.Empty{})
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, fmt.Sprintf("calling grpc jobs: %s", err))
+		return nil, errors.Wrap(err, "calling grpc jobs")
 	}
 
 	items, err := readStream(s)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, fmt.Sprintf("reading grpc jobs stream: %s", err))
+		return nil, errors.Wrap(err, "reading grpc jobs stream")
 	}
 
-	return c.JSON(http.StatusOK, items)
+	return items, nil
 }
